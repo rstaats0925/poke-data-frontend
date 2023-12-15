d82fd66ec5a44658626a3d0ff4e43c6f0012d57a
Ryan Staats <rstaats0927@gmail.com>
2023-12-15 17:01:35 -0600

    set up routes for main page & page that will display cards

--- poke-data/src/components/App/App.js
+++ poke-data/src/components/App/App.js
@@ -1,7 +1,19 @@
 import "./App.css";
+import Main from "../Main/Main";
+import CardContainer from "../CardContainer/CardContainer";
+import { Route } from "react-router-dom";
 
 function App() {
-  return <div className="App">Bulbasaur</div>;
+  return (
+    <div className="App">
+      <Route exact path="/">
+        <Main />
+      </Route>
+      <Route path="/cardContainer">
+        <CardContainer />
+      </Route>
+    </div>
+  );
 }
 
 export default App;
