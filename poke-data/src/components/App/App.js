9e8560a67c19c78128b33ad78e88db28226936fb
Ryan Staats <rstaats0927@gmail.com>
2023-12-15 17:13:10 -0600

    set up routes with switch statement

--- poke-data/src/components/App/App.js
+++ poke-data/src/components/App/App.js
@@ -1,17 +1,19 @@
 import "./App.css";
 import Main from "../Main/Main";
 import CardContainer from "../CardContainer/CardContainer";
-import { Route } from "react-router-dom";
+import { Route, Switch } from "react-router-dom";
 
 function App() {
   return (
     <div className="App">
-      <Route exact path="/">
-        <Main />
-      </Route>
-      <Route path="/cardContainer">
-        <CardContainer />
-      </Route>
+      <Switch>
+        <Route exact path="/">
+          <Main />
+        </Route>
+        <Route path="/cardContainer">
+          <CardContainer />
+        </Route>
+      </Switch>
     </div>
   );
 }
--- poke-data/src/components/Main/Main.js
+++ poke-data/src/components/Main/Main.js
@@ -1,7 +1,18 @@
 import { React } from "react";
 
 function Main() {
-  return <div>Main Page</div>;
+  return (
+    <div>
+      Welcome to Poke data! Here you can look up any of the nearly 1,000
+      pokemon. This is a single location that you can go to in order to look up
+      the statistics of any particular pokemon. Navigate to the pokemon cards by
+      clicking the "see pokemon button". Select the filter you'd like to apply
+      in the "filter" drop down menu & then click "Go!" to apply it & see only
+      the pokemon from that generation, or all of them if you choose. If you
+      know what pokemon specifically that you're looking for use the search bar
+      by typing in the pokemon's name.
+    </div>
+  );
 }
 
 export default Main;
