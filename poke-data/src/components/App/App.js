45a2e30168c7eff30280ef62e1aae9be66e967a8
Ryan Staats <rstaats0927@gmail.com>
2023-12-16 13:53:22 -0600

    added header component

--- poke-data/src/components/App/App.js
+++ poke-data/src/components/App/App.js
@@ -1,11 +1,15 @@
 import "./App.css";
+//Components
 import Main from "../Main/Main";
 import CardContainer from "../CardContainer/CardContainer";
+import Header from "../Header/Header";
+
 import { Route, Switch } from "react-router-dom";
 
 function App() {
   return (
     <div className="App">
+      <Header />
       <Switch>
         <Route exact path="/">
           <Main />
