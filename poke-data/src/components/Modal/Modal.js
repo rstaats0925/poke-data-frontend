632970fc8f6b43dcaafe4aea71a95b71a7dd256b
Ryan Staats <rstaats0927@gmail.com>
2024-01-22 20:39:29 -0600

    added a break point for modal

--- poke-data/src/components/Modal/Modal.js
+++ poke-data/src/components/Modal/Modal.js
@@ -1,18 +1,32 @@
 import "./Modal.css";
-import v from "voca";
-import { gymLeaders } from "../../utils/constants";
 
 export default function Modal({ data }) {
   const imageUrl = data.sprites.other.dream_world.front_default;
-  // const types = [];
+  const attacks = data.attacks.map((a) => (
+    <li>
+      <p className="modal__attack">{a}</p>
+    </li>
+  ));
 
   return (
     <div className="modal">
       <div className="modal__content">
-        <h2 className="modal__name">{data.name.toUpperCase()}</h2>
-        <img className="modal__image" src={imageUrl} alt={data.name} />
-        <p className="modal__number">{`#${data.id}`}</p>
-        <p className="modal__types">{data.types.join(" • ")}</p>
+        <div className="modal__intro">
+          <img className="modal__image" src={imageUrl} alt={data.name}></img>
+          <h2 className="modal__name">{data.name.toUpperCase()}</h2>
+        </div>
+        <div className="modal__data">
+          <div className="modal__general-info">
+            <p className="modal__number">No. {data.id}</p>
+            <p className="modal__types">type(s): {data.types.join(" • ")}</p>
+            <p className="modal__leader">gym leader: {data.leader}</p>
+            <p className="modal__level">level: {data.level}</p>
+          </div>
+          <div>
+            <p className="modal__attacks">Attacks:</p>
+            <ul className="modal__moves">{attacks}</ul>
+          </div>
+        </div>
       </div>
     </div>
   );
