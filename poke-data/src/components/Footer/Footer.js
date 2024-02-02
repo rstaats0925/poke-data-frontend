e7d7217982660bb614f7fa7c1225f5942df5d92c
Ryan Staats <rstaats0927@gmail.com>
2024-02-02 16:13:32 -0600

    added sources  & author to footer

--- poke-data/src/components/Footer/Footer.js
+++ poke-data/src/components/Footer/Footer.js
@@ -2,7 +2,40 @@ import "./Footer.css";
 import React from "react";
 
 function Footer() {
-  return <footer className="Footer">Footer</footer>;
+  return (
+    <footer className="footer">
+      <div className="footer__sources">
+        <p className="footer__sources-header">Sources</p>
+        <a
+          className="footer__source"
+          href="https://pokeapi.co/"
+          target="_blank"
+          rel="noreferrer"
+        >
+          pokeapi
+        </a>
+        <a
+          className="footer__source seribii"
+          href="https://www.serebii.net/gs/gyms.shtml"
+          target="_blank"
+          rel="noreferrer"
+        >
+          serebii
+        </a>
+      </div>
+      <div className="footer__author">
+        <p className="footer__name">Created by Ryan Staats</p>
+        <a
+          className="footer__github"
+          href="https://github.com/rstaats0925"
+          target="_blank"
+          rel="noreferrer"
+        >
+          Github
+        </a>
+      </div>
+    </footer>
+  );
 }
 
 export default Footer;
